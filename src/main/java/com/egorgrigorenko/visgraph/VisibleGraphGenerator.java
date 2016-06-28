36b4c33d2ffbfe3fe97b2fc1206d7c58bb5bb33f
Egor Grigorenko <iopochta@gmail.com>
2016-06-28 19:55:56 +0300

    + init graph with vertices

--- src/main/java/com/egorgrigorenko/visgraph/VisibleGraphGenerator.java
+++ src/main/java/com/egorgrigorenko/visgraph/VisibleGraphGenerator.java
@@ -1,10 +1,16 @@
 package com.egorgrigorenko.visgraph;
 
+import com.vividsolutions.jts.geom.Coordinate;
 import org.geotools.graph.build.line.BasicLineGraphGenerator;
 import org.geotools.graph.structure.Graph;
-import org.opengis.geometry.coordinate.Polygon;
+import com.vividsolutions.jts.geom.Polygon;
+import org.geotools.graph.structure.Node;
+import org.geotools.graph.structure.basic.BasicNode;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 public class VisibleGraphGenerator
 {
@@ -16,6 +22,21 @@ public class VisibleGraphGenerator
     }
 
     public void addObstacles(Collection<Polygon> obstacles) {
+        initGraph(obstacles);
+    }
+
+    @SuppressWarnings("unchecked")
+    private void initGraph(Collection<Polygon> obstacles) {
+        Collection<Node> graphNodes = graph.getNodes();
+
+        for (Polygon p: obstacles) {
+            Set<Coordinate> coords = new HashSet<>(Arrays.asList(p.getCoordinates()));
+            for (Coordinate c: coords) {
+                BasicNode node = new BasicNode();
+                node.setObject(c);
+                graphNodes.add(node);
+            }
+        }
     }
 
     public Graph getGraph() {
--- src/test/java/com/egorgrigorenko/visgraph/VisibleGraphGeneratorTest.java
+++ src/test/java/com/egorgrigorenko/visgraph/VisibleGraphGeneratorTest.java
@@ -1,8 +1,12 @@
 package com.egorgrigorenko.visgraph;
 
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LinearRing;
+import com.vividsolutions.jts.geom.Polygon;
+import org.geotools.geometry.jts.JTSFactoryFinder;
 import org.geotools.graph.structure.Graph;
 import org.junit.Test;
-import org.opengis.geometry.coordinate.Polygon;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +29,41 @@ public class VisibleGraphGeneratorTest
 
         assertEquals(0, graph.getNodes().size());
         assertEquals(0, graph.getEdges().size());
+    }
 
+    @Test
+    public void addObstacles_givenRectAndTriangle_whenGetGraph_thenCorrect() {
+        // given
+        VisibleGraphGenerator graphGenerator = new VisibleGraphGenerator();
+
+        GeometryFactory geometryFactory = JTSFactoryFinder.getGeometryFactory();
+
+        Coordinate[] coords = new Coordinate[] { new Coordinate(20.6364, -133.5453167),
+                new Coordinate(21.0639833, -188.6258167),
+                new Coordinate(4.7045, -117.7161),
+                new Coordinate(4.3393, -134.546),
+                new Coordinate(20.6364, -133.5453167)};
+        LinearRing ring = geometryFactory.createLinearRing(coords);
+        Polygon rect = geometryFactory.createPolygon(ring, null);
+
+        coords = new Coordinate[] { new Coordinate(16.2092500, -102.7966),
+                new Coordinate(9.6997833, -98.9757667),
+                new Coordinate(9.4288833, -108.9827333),
+                new Coordinate(16.2092500, -102.7966)};
+        ring = geometryFactory.createLinearRing(coords);
+        Polygon triangle = geometryFactory.createPolygon(ring, null);
+
+        List<Polygon> obstacles = new ArrayList<>();
+        obstacles.add(rect);
+        obstacles.add(triangle);
+
+        // when
+        graphGenerator.addObstacles(obstacles);
+
+        // then
+        Graph graph = graphGenerator.getGraph();
+
+        assertEquals(7, graph.getNodes().size());
+        assertEquals(12, graph.getEdges().size());
     }
 }
