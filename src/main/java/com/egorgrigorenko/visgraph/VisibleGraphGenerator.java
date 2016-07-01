f84866f49b53d958d65f837eff6f7b03969ca053
Egor Grigorenko <iopochta@gmail.com>
2016-07-01 16:32:58 +0300

    + sortVerticesByAngle

--- src/main/java/com/egorgrigorenko/visgraph/VisibleGraphGenerator.java
+++ src/main/java/com/egorgrigorenko/visgraph/VisibleGraphGenerator.java
@@ -1,28 +1,54 @@
 package com.egorgrigorenko.visgraph;
 
 import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.math.Vector2D;
 import org.geotools.graph.build.line.BasicLineGraphGenerator;
+import org.geotools.graph.structure.Edge;
 import org.geotools.graph.structure.Graph;
 import com.vividsolutions.jts.geom.Polygon;
 import org.geotools.graph.structure.Node;
+import org.geotools.graph.structure.basic.BasicEdge;
 import org.geotools.graph.structure.basic.BasicNode;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class VisibleGraphGenerator
 {
     private Graph graph;
+    private ArrayList<Coordinate> allVertices;
 
     public VisibleGraphGenerator() {
         BasicLineGraphGenerator graphGen = new BasicLineGraphGenerator();
         graph = graphGen.getGraph();
+        allVertices = new ArrayList<>();
     }
 
+    @SuppressWarnings("unchecked")
     public void addObstacles(Collection<Polygon> obstacles) {
+        initObstacleVertices(obstacles);
         initGraph(obstacles);
+
+        for (Coordinate v: allVertices) {
+            Collection<Coordinate> visibleVertices = getVisibleVertices(v, obstacles);
+            Node vNode = getNodeByCoordinate(v);
+
+            for (Coordinate w: visibleVertices) {
+                Node wNode = getNodeByCoordinate(w);
+                Edge e = new BasicEdge(vNode, wNode);
+                graph.getEdges().add(e);
+            }
+        }
+    }
+
+    private void initObstacleVertices(Collection<Polygon> obstacles) {
+        HashSet<Coordinate> coordinates = new HashSet<>();
+
+        for (Polygon p: obstacles) {
+            coordinates.addAll(Arrays.asList(p.getCoordinates()));
+        }
+
+        allVertices.addAll(coordinates);
     }
 
     @SuppressWarnings("unchecked")
@@ -30,13 +56,87 @@ public class VisibleGraphGenerator
         Collection<Node> graphNodes = graph.getNodes();
 
         for (Polygon p: obstacles) {
-            Set<Coordinate> coords = new HashSet<>(Arrays.asList(p.getCoordinates()));
-            for (Coordinate c: coords) {
+            for (Coordinate c: allVertices) {
                 BasicNode node = new BasicNode();
                 node.setObject(c);
                 graphNodes.add(node);
             }
         }
+    }
+
+    private Collection<Coordinate> getVisibleVertices(Coordinate v, Collection<Polygon> obstacles) {
+        ArrayList<Coordinate> visibleVertices = new ArrayList<>();
+
+        Collection<Coordinate> sortedVerticesByAngle = sortVerticesByAngle(v, allVertices);
+
+
+        return visibleVertices;
+    }
+
+    protected Collection<Coordinate> sortVerticesByAngle(Coordinate v, Collection<Coordinate> vertices) {
+
+        class AngleVertex {
+            public double angle;
+            public Coordinate c;
+
+            public AngleVertex(Coordinate p, Coordinate c) {
+                angle = calcAngle(p, c);
+                this.c = c;
+            }
+
+            private double calcAngle(Coordinate pC, Coordinate vC) {
+                Vector2D v1 = new Vector2D(pC, new Coordinate(pC.x + 1, pC.y));
+                Vector2D v2 = new Vector2D(pC, vC);
+
+                double angle = v1.angleTo(v2);
+                if (angle < 0) {
+                    angle += Math.PI * 2;
+                }
+
+                return angle;
+            }
+        };
+
+        Comparator<AngleVertex> comparator = (w1, w2) -> {
+            if (w1.angle > w2.angle) {
+                return -1;
+            } else if (w1.angle < w2.angle) {
+                return 1;
+            }
+
+            Vector2D v1 = new Vector2D(v, w1.c);
+            Vector2D v2 = new Vector2D(v, w2.c);
+
+            return v1.length() < v2.length() ? -1 : 1;
+        };
+
+        TreeSet<AngleVertex> sortedAngleVertices = new TreeSet<>(comparator);
+
+        for (Coordinate c: vertices) {
+            if (v.equals(c)) {
+                continue;
+            }
+            sortedAngleVertices.add(new AngleVertex(v, c));
+        }
+
+        return sortedAngleVertices.stream().map(av -> av.c)
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+//    private double getAngle(Coordinate v, Coordinate w) {
+//        double angle =
+//    }
+
+    @SuppressWarnings("unchecked")
+    private Node getNodeByCoordinate(Coordinate c) {
+        Collection<Node> nodes = graph.getNodes();
+        for (Node n: nodes) {
+            if (n.getObject().equals(c)) {
+                return n;
+            }
+        }
+
+        return null;
     }
 
     public Graph getGraph() {
--- src/test/java/com/egorgrigorenko/visgraph/VisibleGraphGeneratorTest.java
+++ src/test/java/com/egorgrigorenko/visgraph/VisibleGraphGeneratorTest.java
@@ -5,13 +5,17 @@ import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.LinearRing;
 import com.vividsolutions.jts.geom.Polygon;
 import org.geotools.geometry.jts.JTSFactoryFinder;
+import org.geotools.graph.structure.Edge;
 import org.geotools.graph.structure.Graph;
+import org.geotools.graph.structure.Node;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class VisibleGraphGeneratorTest
 {
@@ -31,30 +35,21 @@ public class VisibleGraphGeneratorTest
         assertEquals(0, graph.getEdges().size());
     }
 
-    @Test
-    public void addObstacles_givenRectAndTriangle_whenGetGraph_thenCorrect() {
+//    @Test
+    public void addObstacles_givenTriangle_whenGetGraph_thenCorrect() {
         // given
         VisibleGraphGenerator graphGenerator = new VisibleGraphGenerator();
 
         GeometryFactory geometryFactory = JTSFactoryFinder.getGeometryFactory();
 
-        Coordinate[] coords = new Coordinate[] { new Coordinate(20.6364, -133.5453167),
-                new Coordinate(21.0639833, -188.6258167),
-                new Coordinate(4.7045, -117.7161),
-                new Coordinate(4.3393, -134.546),
-                new Coordinate(20.6364, -133.5453167)};
+        Coordinate t1 = new Coordinate(1, 1);
+        Coordinate t2 = new Coordinate(4, 4);
+        Coordinate t3 = new Coordinate(6, 0);
+        Coordinate[] coords = new Coordinate[] { t1, t2, t3, t1};
         LinearRing ring = geometryFactory.createLinearRing(coords);
-        Polygon rect = geometryFactory.createPolygon(ring, null);
-
-        coords = new Coordinate[] { new Coordinate(16.2092500, -102.7966),
-                new Coordinate(9.6997833, -98.9757667),
-                new Coordinate(9.4288833, -108.9827333),
-                new Coordinate(16.2092500, -102.7966)};
-        ring = geometryFactory.createLinearRing(coords);
         Polygon triangle = geometryFactory.createPolygon(ring, null);
 
         List<Polygon> obstacles = new ArrayList<>();
-        obstacles.add(rect);
         obstacles.add(triangle);
 
         // when
@@ -63,7 +58,98 @@ public class VisibleGraphGeneratorTest
         // then
         Graph graph = graphGenerator.getGraph();
 
-        assertEquals(7, graph.getNodes().size());
-        assertEquals(12, graph.getEdges().size());
+        assertEquals(3, graph.getNodes().size());
+        assertEquals(3, graph.getEdges().size());
+        assertConnections(graph, t1, Arrays.asList(t2, t3));
+        assertConnections(graph, t2, Arrays.asList(t1, t3));
+        assertConnections(graph, t3, Arrays.asList(t1, t2));
+    }
+
+    @SuppressWarnings("unchecked")
+    void assertConnections(Graph graph, Coordinate examCoord, List<Coordinate> coordinates) {
+        boolean isExamNodeFound = false;
+        Collection<Node> nodes = graph.getNodes();
+        for (Node n: nodes) {
+            Coordinate c = (Coordinate) n.getObject();
+            if (c.equals(examCoord)) {
+                isExamNodeFound = true;
+                Collection<Edge> edges = n.getEdges();
+                assertEquals(coordinates.size(), edges.size());
+                ArrayList<Integer> matchedIndices = new ArrayList<>();
+                for (Edge e: edges) {
+                    Node otherNode = e.getOtherNode(n);
+                    Coordinate otherNodeCoord = (Coordinate) otherNode.getObject();
+                    int index = coordinates.indexOf(otherNode);
+                    if (index == -1) {
+                        String errorMsg = String.format("node (%f, %f) isn't connected with " +
+                                "node (%f, %f)", examCoord.x, examCoord.y,
+                                otherNodeCoord.x, otherNodeCoord.y);
+                        assertTrue(errorMsg, false);
+                    } else {
+                        matchedIndices.add(index);
+                    }
+                }
+
+                // check matched indices
+                for (int j = 0; j < coordinates.size(); ++j) {
+                    String errorMsg = String.format("node (%f, %f) have to be connected with " +
+                                    "node (%f, %f)", examCoord.x, examCoord.y,
+                            coordinates.get(j).x, coordinates.get(j).y);
+                    assertTrue(errorMsg, matchedIndices.contains(j));
+                }
+            }
+        }
+        assertTrue(isExamNodeFound);
+    }
+
+    @Test
+    public void sortVerticesByAngle_givenVertices_whenSort_thenSorted() {
+        // given
+        ArrayList<Coordinate> vertices = new ArrayList<>();
+        Coordinate v1 = new Coordinate(1, 1);
+        Coordinate v2 = new Coordinate(4, 4);
+        Coordinate v3 = new Coordinate(6, 0);
+        vertices.addAll(Arrays.asList(v1, v2, v3));
+
+        // when
+        VisibleGraphGenerator graphGenerator = new VisibleGraphGenerator();
+        Collection<Coordinate> sortedVertices1 = graphGenerator
+                .sortVerticesByAngle(v1, vertices);
+
+        Collection<Coordinate> sortedVertices2 = graphGenerator
+                .sortVerticesByAngle(v2, vertices);
+
+        Collection<Coordinate> sortedVertices3 = graphGenerator
+                .sortVerticesByAngle(v3, vertices);
+
+        // assert
+        assertArrayEquals(Arrays.asList(v3, v2).toArray(), sortedVertices1.toArray());
+        assertArrayEquals(Arrays.asList(v3, v1).toArray(), sortedVertices2.toArray());
+        assertArrayEquals(Arrays.asList(v1, v2).toArray(), sortedVertices3.toArray());
+    }
+
+    @Test
+    public void sortVerticesByAngle_oneVerticeOnRay_closestEarlier() {
+        // given
+        ArrayList<Coordinate> vertices = new ArrayList<>();
+        Coordinate v1 = new Coordinate(1, 1);
+        Coordinate v2 = new Coordinate(4, 4);
+        Coordinate v3 = new Coordinate(6, 1);
+        Coordinate v4 = new Coordinate(7, 1);
+        vertices.addAll(Arrays.asList(v1, v2, v3, v4));
+
+        // when
+        VisibleGraphGenerator graphGenerator = new VisibleGraphGenerator();
+        Collection<Coordinate> sortedVertices1 = graphGenerator
+                .sortVerticesByAngle(v1, vertices);
+
+        vertices.clear();
+        vertices.addAll(Arrays.asList(v1, v2, v4, v3));
+        Collection<Coordinate> sortedVertices2 = graphGenerator
+                .sortVerticesByAngle(v1, vertices);
+
+        // then
+        assertArrayEquals(Arrays.asList(v2, v3, v4).toArray(), sortedVertices1.toArray());
+        assertArrayEquals(Arrays.asList(v2, v3, v4).toArray(), sortedVertices2.toArray());
     }
 }
